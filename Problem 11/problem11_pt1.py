92b429d53b9f526f79982d1e8217af03bd9bf7ea
geeick <109990888+geeick@users.noreply.github.com>
2024-12-27 13:45:42 -0300

    part 1 completed. this felt easier than the others

--- Problem 11/problem11_pt1.py
+++ Problem 11/problem11_pt1.py
@@ -12,8 +12,35 @@ def readCode(file_path):
     return code
 
 
+def applyRules(stone):
+    result = []
+    if int(stone) == 0:
+        result = ['1']
+    elif len(stone) % 2 == 0:  
+        stone1, stone2  = int(stone[:len(stone)//2]), int(stone[len(stone)//2:])
+        result = [str(stone1), str(stone2)]
+    else:
+        result = [str(int(stone)*2024)]
+    return result
+
+def blink(code):
+    x=0
+    while x != len(code):
+        results = applyRules(code[x])
+        code.pop(x)
+        for result in results:
+            code.insert(x, result)
+            x=x+1
+    return code
+
+
 file_path = "/Users/georgiaeick/Library/Mobile Documents/com~apple~TextEdit/Documents/AdventOfCode_Problem11.txt"
 file_path_example = "/Users/georgiaeick/Library/Mobile Documents/com~apple~TextEdit/Documents/AdventOfCode_Problem11Ex.txt"
 
 
-print(readCode(file_path))
+line = readCode(file_path)
+
+for x in range(25):
+    result = (blink(line))
+    print(len(result))
+
