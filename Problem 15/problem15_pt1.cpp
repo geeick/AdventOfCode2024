4465abfb22a1d4fc595c3a21976b25b7a7c7a7ca
geeick <109990888+geeick@users.noreply.github.com>
2025-01-19 11:48:44 -0800

    This was so much easier than I initially tried to make.

--- Problem 15/problem15_pt1.cpp
+++ Problem 15/problem15_pt1.cpp
@@ -1,7 +1,4 @@
 #include "globals.h"
-#include "Box.h"
-#include "Grid.h"
-#include "Robot.h"
 #include <vector>
 
 int translateCommand(char command);
@@ -14,43 +11,119 @@ std::vector<std::vector<char>> map = instructions.getGrid(); //grid is a grid ob
 
 
 int main() {
+    int total = 0;
+
     for (int cmd = 0; cmd < commands.size(); cmd++) {
+
+        bool objectMoved = false;
         char command = commands[cmd];
+
         for (int i = 0; i < map.size(); i++) {
+            if (objectMoved) break;
             for (int j = 0; j < map[0].size(); j++) {
                 if (map[i][j] == '@') {
+                    objectMoved = true;
                     moveObject(command, i, j);
+                    break;
                 }
             }
         }
+
+    }
+    for (int i = 0; i < map.size(); i++) {
+        for (int j = 0; j < map[0].size(); j++) {
+            if (map[i][j]== 'O') {
+                total += ((100*i)+j);
+            }
+        }
+    }
+    for (int i = 0; i < map.size(); i++) {
+        for (int j = 0; j < map[0].size(); j++) {
+            std::cout<<map[i][j];
+        }
+        std::cout<<std::endl;
+
     }
 
+    std::cout << total << std::endl;
 
     return 0;
 }
 
 bool moveObject(char dir, int row, int col) {
-        int command = translateCommand(dir);
+    int command = translateCommand(dir);
+    char temp =  map[row][col];
 
-        switch (command) {
-            default: break;
-            case 1:
-                if (row > 0 && map[row -1][col] == '#') {
-                    return moveObject(dir, row - 1, col);
-                }
-                if (row <= 0){
-                    return false;
+    switch (command) {
+        default: break;
+        case 1:
+            if (map[row -1][col] == '#') {
+                return false;
+            }
 
-                map[row-1][col] = '@';
+            if (map[row -1][col] == 'O') {
+                moveObject(dir, row - 1, col);
+            }
+
+            if (map[row -1][col] == '.') {
+                map[row-1][col] = temp;
                 map[row][col] = '.';
-            case 2:
-                map[i][j] = '#';
-            case 3:
-                map[i][j] = '@';
-            case 4:
-                map[i][j] = '#';
-        }
+                return true;
+            }
+
+            return true;
+
+        case 2:
+            if (map[row][col +1] == '#') {
+                return false;
+            }
+
+            if (map[row][col +1] == 'O') {
+                moveObject(dir, row, col + 1);
+            }
+
+            if (map[row][col+1] == '.') {
+                map[row][col + 1] = temp;
+                map[row][col] = '.';
+            }
+
+            return true;
+
+        case 3:
+            if (map[row +1][col] == '#') {
+                    return false;
+                }
+
+            if (map[row +1][col] == 'O') {
+                moveObject(dir, row + 1, col);
+            }
+
+            if (map[row +1][col] == '.') {
+                map[row+1][col] = temp;
+                map[row][col] = '.';
+            }
+
+            return true;
+
+        case 4:
+            if (map[row][col -1] == '#') {
+                return false;
+            }
+
+            if (map[row][col -1] == 'O') {
+                moveObject(dir, row, col - 1);
+            }
+
+            if (map[row][col -1] == '.') {
+                map[row][col - 1] = temp;
+                map[row][col] = '.';
+            }
+
+        return true;
     }
+
+    return true;
+}
 
 int translateCommand(char command) {
     if (command == '^')
@@ -63,8 +136,5 @@ int translateCommand(char command) {
         return 4;
 
     return 0;
-}
-
-
 }
 
